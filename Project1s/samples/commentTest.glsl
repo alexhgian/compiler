03631434e108ed3e2aa081a31722292150afb9aa
Nazior Rahman <naziorrahman.91@gmail.com>
2017-01-22 14:54:59 -0800

    Added test file, /samples and modified the tokens in Scanner.l in order

---
+++ Project1s/samples/commentTest.glsl
@@ -0,0 +1,24 @@
+// this is a comment
+// comment
+// so many
+// hello
+// la la la
+uint 12345u
+0.123456
+123e3
+12345
+// more comment
+// comment comment comment
+// blah blah blah
+// ok
+vec2 vec2 vec2
+/* COMMENT COMMENT COMMENT
+I AM A COMMENT
+************ * * * * **  *    /
+, ` ||| /*
+BLAH BLAH BLAH
+HELLO
+HELLO
+THIS IS LONG */
+hello bye bye
+/* BLAHB LA a;sdkjfas;dfjs;dfj *       /
