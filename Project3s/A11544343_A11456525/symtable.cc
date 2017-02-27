da5639b82f82780b99534af5ce2ab69966bb0801
Alex Gian <agian@ucsd.edu>
2017-02-26 23:40:19 -0800

    submit zip

---
+++ Project3s/A11544343_A11456525/symtable.cc
@@ -0,0 +1,115 @@
+/*
+ * file: symtable.cc
+ * author: Alex Gian, Nazior Rahman
+ */
+#include "symtable.h"
+#include "utility.h"
+
+ /**
+ * ScopedTable
+ * TODO:
+ * [] destructor
+ * [x] constructor
+ * [x] insert
+ * [x] remove
+ * [x] find
+ */
+ ScopedTable::ScopedTable(){
+     breakable = false;
+     continuable = false;
+    //  SetDebugForKey("symtable", false);
+    //  PrintDebug("symtable", "Init ScopeTable - symbols.size() %d\n", (int)symbols.size());
+ }
+
+ ScopedTable::~ScopedTable(){
+     // Do Clean Up
+ }
+
+void ScopedTable::insert(Symbol &sym){
+    // When inserting it should check for redeclaration
+    // using Symbol *find(const char *name);
+    // to see if we are redeclaring in the same scope
+    // symbol.
+    SymbolIterator it = symbols.find(sym.name);
+
+    if( it == symbols.end() ){
+        // printf("Inserting new decl: %s\n", sym.name);
+        symbols[sym.name] = sym;
+    } else {
+        // throw redeclartion error here
+        Decl *tmpDecl = this->find(sym.name)->decl;
+        ReportError::DeclConflict(sym.decl, tmpDecl);
+        symbols[sym.name] = sym;
+    }
+
+
+}
+
+void ScopedTable::remove(Symbol &sym){
+    symbols.erase(sym.name);
+}
+
+Symbol *ScopedTable::find(const char *name){
+    // Search for the symbol
+    SymbolIterator it = symbols.find(name);
+    // dereference the iterator's element (Symbol)
+    return &it->second;
+}
+
+/**
+* SymbolTable
+* TODO:
+* [] destructor
+* [x] constructor
+* [] push
+* [] pop
+* [x] insert
+* [x] remove
+* [x] find
+*/
+SymbolTable::SymbolTable(){
+    // Create a new scope (global scope)
+    this->push();
+}
+
+/**
+* insert
+* Adds a symbol into the symbol table (map)
+*/
+void SymbolTable::insert(Symbol &sym){
+    SetDebugForKey("symtable", false);
+    // PrintDebug("symtable","Function\n");
+    if(sym.kind == E_FunctionDecl){
+        PrintDebug("symtable","Function: %s\n", sym.name);
+        lastFn =  dynamic_cast<FnDecl*>(sym.decl);;
+    }
+    ScopedTable *st = tables.back();
+    st->insert(sym);
+}
+/**
+*
+*/
+void SymbolTable::remove(Symbol &sym){
+    ScopedTable *st = tables.back();
+    st->remove(sym);
+}
+
+void SymbolTable::push(){
+    tables.push_back(new ScopedTable());
+}
+
+void SymbolTable::pop(){
+    tables.pop_back();
+}
+
+Symbol *SymbolTable::find(const char* name){
+    ScopedTable *st = tables.back();
+    return st->find(name);
+    // for(int i = 0; i < tables.size(); i++){
+    //     Symbol * res = tables[i]->find(name);
+    //     if(res){
+    //         return res;
+    //     }
+    // }
+    // return NULL;
+}
---
+++ Project3s/A11544343_A11456525/symtable.h
@@ -0,0 +1,109 @@
+/**
+ * File: symtable.h
+ * -----------
+ *  This file defines a class for symbol table and scoped table table.
+ *
+ *  Scoped table is to hold all declarations in a nested scope. It simply
+ *  uses the standard C++ map.
+ *
+ *  Symbol table is implemented as a vector, where each vector entry holds
+ *  a pointer to the scoped table.
+ */
+
+#ifndef _H_symtable
+#define _H_symtable
+
+#include <map>
+#include <vector>
+#include <iostream>
+#include <string.h>
+#include "errors.h"
+
+using namespace std;
+
+class Decl;
+class Stmt;
+
+enum EntryKind {
+  E_FunctionDecl,
+  E_VarDecl,
+};
+
+struct Symbol {
+  char *name;
+  Decl *decl;
+  EntryKind kind;
+  int someInfo;
+
+  Symbol() : name(NULL), decl(NULL), kind(E_VarDecl), someInfo(0) {}
+  Symbol(char *n, Decl *d, EntryKind k, int info = 0) :
+        name(n),
+        decl(d),
+        kind(k),
+        someInfo(info) {}
+};
+
+struct lessStr {
+  bool operator()(const char* s1, const char* s2) const
+  { return strcmp(s1, s2) < 0; }
+};
+
+typedef map<const char *, Symbol, lessStr>::iterator SymbolIterator;
+typedef pair<const char *, Symbol> SymPair;
+
+class ScopedTable {
+  map<const char *, Symbol, lessStr> symbols;
+  Symbol * lastSymbol;
+  bool breakable;
+  bool continuable;
+  public:
+    ScopedTable();
+    ~ScopedTable();
+
+    void insert(Symbol &sym);
+    void remove(Symbol &sym);
+    Symbol *find(const char *name);
+    Symbol *last(){ return lastSymbol; };
+
+    bool isBreakable(){ return breakable; };
+    void setBreakable(bool val){ breakable = val; };
+
+    bool isContinuable(){ return continuable; };
+    void setContinuable(bool val){ continuable = val; };
+};
+
+class SymbolTable {
+  std::vector<ScopedTable *> tables;
+  // ScopeTable* currentScope;
+  FnDecl * lastFn;
+  public:
+    SymbolTable();
+    ~SymbolTable();
+
+    void push();
+    void pop();
+
+    void insert(Symbol &sym);
+    void remove(Symbol &sym);
+    Symbol *find(const char *name);
+
+    FnDecl* getLastFn(){return lastFn; }
+
+    bool isBreakable() { return tables.back()->isBreakable(); }
+    void setBreakable(bool val){ tables.back()->setBreakable(val); }
+
+    bool isContinuable() { return tables.back()->isContinuable(); }
+    void setContinuable(bool val){ tables.back()->setContinuable(val); }
+};
+
+class MyStack {
+    vector<Stmt *> stmtStack;
+
+  public:
+    void push(Stmt *s) { stmtStack.push_back(s); }
+    void pop()         { if (stmtStack.size() > 0 ) stmtStack.pop_back(); }
+    bool insideLoop();
+    bool insideSwitch();
+};
+
+#endif
